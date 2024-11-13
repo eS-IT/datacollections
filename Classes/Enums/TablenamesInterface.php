1f8635bc3484005b57cf85fdf0db61f216650d4c
Patrick Froch <p.froch@netgroup.de>
2024-11-13 09:03:07 +0100

    Added: Kompatibilitätslayer für Anwenungen, die bis jetzt Version < 2.0 verwenden

---
+++ Classes/Enums/TablenamesInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @since       13.11.2024 - 08:59
+ *
+ * @author      Patrick Froch <info@netgroup.de>
+ *
+ * @see         http://www.netgroup.de
+ *
+ * @copyright   NetGroup GmbH 2024
+ * @license     EULA
+ */
+
+declare(strict_types=1);
+
+namespace Esit\Datacollections\Classes\Enums;
+
+/**
+ * Kompatibilitätslayer für Applikationen, die bis jetzt Version < 2.0 verwenden.
+ */
+interface TablenamesInterface extends \Esit\Valueobjects\Classes\Database\Enums\TablenamesInterface
+{
+}
